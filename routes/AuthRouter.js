ea40e11acacf1f31fcd5588d55db5350c9cc0dc8
Erica Harmon <ericalaurenharmon@gmail.com>
2024-02-08 13:53:02 -0500

    rename auth

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
-const controller = require('../controllers/duck')
+const controller = require('../controllers/auth')
 
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
-// router.put('/update-password/:user_id', controller.UpdatePassword)
 
 module.exports = router
