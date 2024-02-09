7474050306279c643f32f77a83f14fb5ce0ea03b
Erica Harmon <ericalaurenharmon@gmail.com>
2024-02-09 09:55:48 -0500

    userName updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,13 +3,13 @@ const middleware = require('../middleware')
 
 const Register = async (req, res) => {
     try {
-        const { email, password, name } = req.body
+        const { email, password, userName } = req.body
         let passwordDigest = await middleware.hashPassword(password)
         let existingUser = await User.findOne({ email })
         if (existingUser) {
             return res.status(400).send("A user with that email has already been registered!")
         } else {
-            const user = await User.create({ name, email, passwordDigest })
+            const user = await User.create({ userName, email, passwordDigest })
             res.send(user)
         }
     } catch (error) {
